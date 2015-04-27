193ceec95d8516e111d103912fcc4b342f0bd669
moro <moronatural@gmail.com>
2015-04-28 08:51:31 +0900

    Refactoring

--- lib/hash_traverse/traverser.rb
+++ lib/hash_traverse/traverser.rb
@@ -1,36 +1,46 @@
 module HashTraverse
   class Traverser
-    FetchResult = Struct.new(:value, :is_fetched)
+
+    # TODO English translation: currently lang:ja
+    #
+    # NOTE 値が falsy object だった場合にはそれを取得するため、safe_fetch では
+    #      単純な真偽以外で判定する必要あり
+    #      テキトウなオブジェクトを返すようにし、その equality で結果有無を判定する
+    @@missing_token = Object.new
 
     def initialize(hash)
       @hash = hash
     end
 
-    def traverse(*keys, &missing)
+    def traverse(*keys, &missing_handler)
       found_keys   = []
 
       keys.inject(@hash) do |parent, key|
-        if (result = safe_fetch(parent, key)).is_fetched
-          found_keys << key
-          result.value
+        if (result = safe_fetch(parent, key)).equal?(@@missing_token)
+          break handle_missing(missing_handler, found_keys, parent)
         else
-          handle_missing(missing, found_keys, parent)
+          found_keys << key
+          result
         end
       end
     end
 
     private
 
-    def handle_missing(missing, keys, parent)
-      missing ? missing.call(keys, parent) : nil
+    def handle_missing(handler, keys, parent)
+      handler ? handler.call(keys, parent) : nil
     end
 
     def safe_fetch(parent, key)
-      if parent.respond_to?(:has_key?) && parent.respond_to?(:fetch)
-        FetchResult.new(parent.fetch(key) { nil }, parent.has_key?(key))
+      if hashy?(parent) && parent.has_key?(key)
+        parent.fetch(key)
       else
-        FetchResult.new(nil, false)
+        @@missing_token
       end
+    end
+
+    def hashy?(object)
+      object.respond_to?(:has_key?) && object.respond_to?(:fetch)
     end
   end
 end
