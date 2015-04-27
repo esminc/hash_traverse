cb5959ca990d5792fe9d37b3a028c02bea39aec1
moro <moronatural@gmail.com>
2015-04-28 08:34:06 +0900

    Refactoring

--- lib/hash_traverse/traverser.rb
+++ lib/hash_traverse/traverser.rb
@@ -1,21 +1,20 @@
 module HashTraverse
   class Traverser
+    FetchResult = Struct.new(:value, :is_fetched)
+
     def initialize(hash)
       @hash = hash
     end
 
     def traverse(*keys, &missing)
-      found_keys = []
+      found_keys   = []
 
       keys.inject(@hash) do |parent, key|
-        begin
-          if parent.respond_to?(:fetch)
-            parent.fetch(key).tap { found_keys << key }
-          else
-            handle_missing(missing, found_keys, parent)
-          end
-        rescue KeyError
-          break handle_missing(missing, found_keys, parent)
+        if (result = safe_fetch(parent, key)).is_fetched
+          found_keys << key
+          result.value
+        else
+          handle_missing(missing, found_keys, parent)
         end
       end
     end
@@ -27,7 +26,11 @@ module HashTraverse
     end
 
     def safe_fetch(parent, key)
-      parent.respond_to?(:fetch) ? parent.fetch(key) : nil
+      if parent.respond_to?(:has_key?) && parent.respond_to?(:fetch)
+        FetchResult.new(parent.fetch(key) { nil }, parent.has_key?(key))
+      else
+        FetchResult.new(nil, false)
+      end
     end
   end
 end
